cb7f491a27e835c8a341a5a5a1a79ba5d32963c2
Egor_Ovs <ega.firefox@gmail.com>
2025-02-27 18:45:38 +0300

    Circle no UI

--- main.py
+++ main.py
@@ -7,18 +7,14 @@ import random
 class Circle(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
-
-
-    def initUI(self):
         self.setGeometry(100, 100, 350, 400)
         self.setWindowTitle('Окружности')
         self.do_paint = False
+
         self.trick_button = QPushButton('Окружности', self)
         self.trick_button.move(140, 20)
         self.trick_button.resize(self.trick_button.sizeHint())
         self.trick_button.clicked.connect(self.paint)
-        self.sp_color = [QColor(255, 255, 0), QColor(255, 0, 255), QColor(255, 0, 0), QColor(0, 255, 0), QColor(0, 0, 255)]
 
     def paintEvent(self, event):
         if self.do_paint:
@@ -33,9 +29,8 @@ class Circle(QWidget):
         self.update()
 
     def draw_circle(self, qp):
-        color = random.randint(0, 4)
-        print(color)
-        qp.setBrush(self.sp_color[color])
+        r, g, b = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+        qp.setBrush(QColor(r, g, b))
         x = random.randint(20, 100)
         qp.drawEllipse(90, 100, x, x)
 
